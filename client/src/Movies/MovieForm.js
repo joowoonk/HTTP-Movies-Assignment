169649f3b39c97ce0c0db21cc0521e86a0751939
Golden Eagle <joowoon.kang@biola.edu>
2020-04-22 22:13:34 -0700

    added updated movie button

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -26,35 +26,51 @@ const MovieForm = () => {
       });
   }, [id]);
 
+  const handleChange = (e) => {
+    e.persist();
+    let value = e.target.value;
+    if (e.target.name === "metascore") {
+      value = parseInt(value, 10);
+    }
+    setMovie({
+      ...movie,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const updateForm = (e) => {
+    console.log("submitbutoon working");
+  };
+
   return (
     <div>
       <h2>Update This Movie</h2>
-      <form>
+      <form onSubmit={updateForm}>
         <input
           type="text"
           name="title"
-          // onChange={handleChange}
+          onChange={handleChange}
           placeholder="Title"
           value={movie.title}
         />
         <input
           type="text"
           name="director"
-          // onChange={handleChange}
+          onChange={handleChange}
           placeholder="Director"
           value={movie.director}
         />
         <input
           type="text"
           name="metascore"
-          // onChange={handleChange}
+          onChange={handleChange}
           placeholder="Metascore"
           value={movie.metascore}
         />
         <input
           type="text"
           name="stars"
-          // onChange={handleChange}
+          onChange={handleChange}
           placeholder="Stars"
           value={movie.stars}
         />
